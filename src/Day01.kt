59749c9ac210757c3e4ff5dc6be138c29f7561de
DmytroRuban <veliymolfar@gmail.com>
2024-12-02 12:59:34 +0200

    day01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,51 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val leftList = mutableListOf<Int>()
+        val rightList = mutableListOf<Int>()
+
+        input.map { line ->
+            val (leftNumber, rightNumber) = line.splitToNumbers()
+            leftList.add(leftNumber)
+            rightList.add(rightNumber)
+        }
+
+        leftList.sort()
+        rightList.sort()
+
+        val sum = leftList.mapIndexed { index, leftNumber ->
+            abs(leftNumber - rightList[index])
+        }.sum()
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val leftList = mutableListOf<Int>()
+        val rightList = mutableListOf<Int>()
+
+        input.map { line ->
+            val (leftNumber, rightNumber) = line.splitToNumbers()
+            leftList.add(leftNumber)
+            rightList.add(rightNumber)
+        }
+
+        val similarity = leftList.sumOf { leftNumber ->
+            leftNumber * rightList.filter { rightNumber -> rightNumber == leftNumber }.size
+        }
+
+        return similarity
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    check(part1(listOf("3   7")) == 4)
+    check(part1(listOf("9   3")) == 6)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,10 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * Splits [String] line to list of numbers.
+ */
+fun String.splitToNumbers(): List<Int> {
+    return this.split("\\s+".toRegex()).map { it.toInt() }
+}
